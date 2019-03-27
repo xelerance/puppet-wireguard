c651c1fdcf98c96e5302874fd7b482e261b000c2
Simon Deziel <simon@sdeziel.info>
2019-03-27 15:33:10 -0400

    wireguard: apply systemd::network changes even if $systemd::manage_networkd is false
    Signed-off-by: Simon Deziel <simon@sdeziel.info>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,18 @@
 #
 class wireguard (
-  String $ppa = 'ppa:wireguard/wireguard',
+  String  $ppa              = 'ppa:wireguard/wireguard',
+  Boolean $restart_networkd = true,
 ) {
   apt::ppa { $ppa: }
   package { 'wireguard':
     require => Apt::Ppa[$ppa],
+  }
+
+  # apply systemd::network changes even if $systemd::manage_networkd is false
+  if $restart_networkd and ! $systemd::manage_networkd {
+    service { 'systemd-networkd': }
+    Systemd::Network <| |>
+    ~> Class['systemd::systemctl::daemon_reload']
+    ~> Service['systemd-networkd']
   }
 }
