0515b2f84debf96ff265317d58872418068fe3e2
Simon Deziel <simon@sdeziel.info>
2020-03-20 13:02:50 -0400

    Ubuntu 20.04 comes with builtin support for Wireguard

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,16 @@
 #
 class wireguard (
   String  $ppa              = 'ppa:wireguard/wireguard',
+  Boolean $force_ppa        = false,
   Boolean $restart_networkd = true,
 ) {
-  apt::ppa { $ppa: }
-  package { 'wireguard':
-    require => Apt::Ppa[$ppa],
+  if $force_ppa or (versioncmp($::os['release']['full'], '20.04') < 0) {
+    apt::ppa { $ppa: }
+    package { 'wireguard':
+      require => Apt::Ppa[$ppa],
+    }
+  } else {
+    package { 'wireguard': }
   }
 
   # apply systemd::network changes even if $systemd::manage_networkd is false
