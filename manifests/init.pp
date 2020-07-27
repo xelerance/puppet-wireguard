543e2611be350e6a468f0d665082d9291d25b49f
Simon Deziel <simon@sdeziel.info>
2020-07-27 11:25:33 -0400

    Wireguard PPA no longer needed now that Ubuntu archives include the wireguard packages (and kernels ship wireguard.ko)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,8 @@
 #
 class wireguard (
-  String  $ppa              = 'ppa:wireguard/wireguard',
-  Boolean $force_ppa        = false,
   Boolean $restart_networkd = true,
 ) {
-  if $force_ppa or (versioncmp($::os['release']['full'], '20.04') < 0) {
-    apt::ppa { $ppa: }
-    package { 'wireguard':
-      require => Apt::Ppa[$ppa],
-    }
-  } else {
-    # avoids pulling in wireguard-dkms
-    package { 'wireguard-tools':
-      install_options => ['--no-install-recommends'],
-    }
-  }
+  package { 'wireguard': }
 
   # apply systemd::network changes even if $systemd::manage_networkd is false
   if $restart_networkd and ! $systemd::manage_networkd {
